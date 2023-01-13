d910ff750514c276e41b75821ab64378d6f6d371
Fieldplayz <400042113@st.roc.a12.nl>
2023-01-13 19:24:38 +0100

    hj

---
+++ RoomScape/Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public enum Puzzles
+{
+    Combination,
+    Combination_Code,
+    Piano,
+    Piano_Order,
+
+}
+
+public class PuzzleManager : MonoBehaviour
+{
+    [SerializeField] Puzzles puzzle;
+
+    [Header("Combination")]
+    [SerializeField] TMP_Text digitLeft;
+    [SerializeField] TMP_Text digitMiddle;
+    [SerializeField] TMP_Text digitRight;
+    [SerializeField] int digitLeftKey;
+    [SerializeField] int digitMiddleKey;
+    [SerializeField] int digitRightKey;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void DigitLeft()
+    {
+        int digit1 = int.Parse(digitLeft.text);
+        int digit2 = int.Parse(digitMiddle.text);
+        int digit3 = int.Parse(digitRight.text);
+
+        if (digit1 >= 9)
+        {
+            digit1 = 0;
+            digitLeft.text = digit1.ToString();
+        }
+        else
+        {
+            digit1++;
+            digitLeft.text = digit1.ToString();
+        }
+
+        if (digit1 == digitLeftKey && digit2 == digitMiddleKey && digit3 == digitRightKey)
+        {
+            digit1 = 0;
+            digitLeft.text = digit1.ToString();
+            digit2 = 0;
+            digitMiddle.text = digit2.ToString();
+            digit3 = 0;
+            digitRight.text = digit3.ToString();
+        }
+    }
+
+    public void DigitMiddle()
+    {
+        int digit1 = int.Parse(digitLeft.text);
+        int digit2 = int.Parse(digitMiddle.text);
+        int digit3 = int.Parse(digitRight.text);
+
+        if (digit2 >= 9)
+        {
+            digit2 = 0;
+            digitMiddle.text = digit2.ToString();
+        }
+        else
+        {
+            digit2++;
+            digitMiddle.text = digit2.ToString();
+        }
+
+        if (digit1 == digitLeftKey && digit2 == digitMiddleKey && digit3 == digitRightKey)
+        {
+            digit1 = 0;
+            digitLeft.text = digit1.ToString();
+            digit2 = 0;
+            digitMiddle.text = digit2.ToString();
+            digit3 = 0;
+            digitRight.text = digit3.ToString(); ;
+        }
+    }
+
+    public void DigitRight()
+    {
+        int digit1 = int.Parse(digitLeft.text);
+        int digit2 = int.Parse(digitMiddle.text);
+        int digit3 = int.Parse(digitRight.text);
+
+        if (digit3 >= 9)
+        {
+            digit3 = 0;
+            digitRight.text = digit3.ToString();
+        }
+        else
+        {
+            digit3++;
+            digitRight.text = digit3.ToString();
+        }
+
+        if (digit1 == digitLeftKey && digit2 == digitMiddleKey && digit3 == digitRightKey)
+        {
+            digit1 = 0;
+            digitLeft.text = digit1.ToString();
+            digit2 = 0;
+            digitMiddle.text = digit2.ToString();
+            digit3 = 0;
+            digitRight.text = digit3.ToString();
+        }
+    }
+}
